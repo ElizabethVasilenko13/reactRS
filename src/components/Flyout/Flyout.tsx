d7467f4f7e6eba8ce403bd960693dff9b41ad1a7
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-07-25 19:49:19 +0200

    feat: implement flyout element

---
+++ src/components/Flyout/Flyout.tsx
@@ -0,0 +1,39 @@
+import { useAppSelector } from '@store/store';
+import { useDispatch } from 'react-redux';
+import { unselectAll } from '@store/cards/cards.slice';
+import { convertToCSV } from '@utils/convert-to-CSV';
+import { CharacterInfo } from '@models/rick-and-morty-api.interface';
+import styles from './Flyout.module.scss';
+
+const Flyout: React.FC = () => {
+  const dispatch = useDispatch();
+  const selectedCards = useAppSelector((state) => state.cards.selectedCards);
+  const selectedCardsCount = Object.keys(selectedCards).length;
+
+  if (selectedCardsCount === 0) return null;
+
+  const handleUnselectAll = () => {
+    dispatch(unselectAll());
+  };
+
+  const dataTransform = (item: CharacterInfo) => [item.name, item.species, `${window.location.origin}/${item.id}`];
+  const csvUrl = convertToCSV(selectedCards, dataTransform);
+
+  const downloadFileName = `${selectedCardsCount}_character${selectedCardsCount > 1 ? 's' : ''}.csv`;
+
+  return (
+    <div className={styles.flyout}>
+      <button type="button" onClick={handleUnselectAll}>
+        Unselect All
+      </button>
+      <p>
+        {selectedCardsCount} item{selectedCardsCount > 1 ? 's' : ''} selected
+      </p>
+      <a href={csvUrl} download={downloadFileName}>
+        Download
+      </a>
+    </div>
+  );
+};
+
+export default Flyout;
