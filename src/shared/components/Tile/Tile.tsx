ad33cc4a232f238ff9684668fa2a77e3fee9da0c
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-08-16 15:51:03 +0200

    feat: add tile file

---
+++ src/shared/components/Tile/Tile.tsx
@@ -0,0 +1,36 @@
+import { FormSubmission } from '@models/form.interface';
+import styles from './Tile.module.scss';
+import classnames from 'classnames';
+
+type TileProps = {
+  submissionInfo: FormSubmission;
+};
+
+const Tile: React.FC<TileProps> = ({ submissionInfo }) => {
+  return (
+    <div className={classnames(styles.tile, { [styles.updated]: submissionInfo.isUpdated })}>
+      <h3>Submission Details</h3>
+      <p>
+        <strong>Name:</strong> {submissionInfo.name}
+      </p>
+      <p>
+        <strong>Age:</strong> {submissionInfo.age}
+      </p>
+      <p>
+        <strong>Email:</strong> {submissionInfo.email}
+      </p>
+      <p>
+        <strong>Gender:</strong> {submissionInfo.gender}
+      </p>
+      <p>
+        <strong>Picture:</strong>
+        <img src={submissionInfo.image} alt="Image" />
+      </p>
+      <p>
+        <strong>Country:</strong> {submissionInfo.country}
+      </p>
+    </div>
+  );
+};
+
+export default Tile;
